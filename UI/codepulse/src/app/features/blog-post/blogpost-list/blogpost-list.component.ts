f8c5941fa80a21bfde4df48b92644980e3ff0f5d
Pedro Oliveira <pedrooli98@outlook.com>
2023-11-17 12:03:01 -0300

    feat: blog posts list

--- UI/codepulse/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
+++ UI/codepulse/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post.model';
+import { BlogPostsService } from '../services/blogposts-services.service';
 
 @Component({
   selector: 'app-blogpost-list',
   templateUrl: './blogpost-list.component.html',
   styleUrls: ['./blogpost-list.component.css']
 })
-export class BlogpostListComponent {
+export class BlogpostListComponent implements OnInit{
+  blogPosts$?: Observable<BlogPost[]>
+
+  constructor(private service: BlogPostsService){}
+  
+  ngOnInit(): void {
+    this.blogPosts$ = this.service.getAllBlogPosts();
+  }
 
 }
